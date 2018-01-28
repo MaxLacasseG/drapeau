e3634e764002c6c38d3c68b3ccbab58edc78d750
Maxime Lacasse Germain <max.lacasse.g@gmail.com>
2018-01-28 13:42:11 -0500

    Création d'un prototype

---
+++ prototype_socket/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+app.use('/css', express.static(__dirname+'/css'));
+app.use('/js', express.static(__dirname+'/js'));
+app.use('/medias', express.static(__dirname+'/medias'));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8081,function(){ // Listens to port 8081
+    console.log('Listening on '+server.address().port);
+});
